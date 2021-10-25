717f39166f2c2e69f888fca1bc58a5bd6cdbaab3
Adityachess <adityadone9398@gmail.com>
2021-10-26 00:23:49 +0530

    Added:User need to enter valid last name

--- src/main/java/com/bridzelabz/userregistrationprogram/UserRegistration.java
+++ src/main/java/com/bridzelabz/userregistrationprogram/UserRegistration.java
@@ -5,16 +5,23 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
 	public static final Scanner sc = new Scanner(System.in);
-	public final String firstNanmePattern = "^[A-Z]{1}[a-z]{2,}$";
+	public final String firstNamePattern = "^[A-Z]{1}[a-z]{2,}$";
+	public final String lastNamePattern = "^[A-Z]{1}[a-z]{2,}$";
 
 	// This method checks if the entered first name is valid
 	public boolean validateFirstName(String fname) {
-		Pattern pattern = Pattern.compile(firstNanmePattern);
+		Pattern pattern = Pattern.compile(firstNamePattern);
 		return pattern.matcher(fname).matches();
+	}
+
+	public boolean validateLastName(String lname) {
+		Pattern pattern = Pattern.compile(lastNamePattern);
+		return pattern.matcher(lname).matches();
 	}
 
 	// This is the main function
 	public static void main(String[] args) {
+
 		System.out.println("Enter First Name: ");
 		String fname = sc.next();
 		UserRegistration obj = new UserRegistration();
@@ -22,6 +29,14 @@ public class UserRegistration {
 			System.out.println("First Name Valid");
 		} else {
 			System.out.println("First Name Invalid");
+		}
+
+		System.out.println("\nEnter Last Name: ");
+		String lname = sc.next();
+		if (obj.validateFirstName(lname)) {
+			System.out.println("Last Name Valid");
+		} else {
+			System.out.println("Last Name Invalid");
 		}
 	}
 }
