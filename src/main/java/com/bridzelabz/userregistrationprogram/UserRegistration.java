e54279c78589595d1c19faf835cc2b3407b55d43
Adityachess <adityadone9398@gmail.com>
2021-10-26 00:59:49 +0530

    Added:Validate user using junit test

--- src/main/java/com/bridzelabz/userregistrationprogram/UserRegistration.java
+++ src/main/java/com/bridzelabz/userregistrationprogram/UserRegistration.java
@@ -9,7 +9,7 @@ public class UserRegistration {
 	public final String lastNamePattern = "^[A-Z]{1}[a-z]{2,}$";
 	public final String emailPattern = "[a-z0-9_]*[.a-z0-9_]*?@[a-z]*.[a-z]*[.a-z]?$";
 	public final String phonePattern = "^[0-9]{2}[0-9]{10}$";
-	public final String passwordPatternSecond = "^(?=.*[A-Z]).{8,}$";
+	public final String passwordPattern = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&-+=()]).{8,}$";
 
 	/**
 	 * This method checks if the entered first name is valid
@@ -44,8 +44,20 @@ public class UserRegistration {
 	 * This method checks if the entered password is valid
 	 */
 	public boolean validatePassword(String password) {
-		Pattern pattern = Pattern.compile(passwordPatternSecond);
+		Pattern pattern = Pattern.compile(passwordPattern);
 		return pattern.matcher(password).matches();
+	}
+
+	/**
+	 * Method for printing validate email output;
+	 */
+	public void emailTest(String email) {
+		System.out.print(email + "->");
+		if (validateEmail(email)) {
+			System.out.printf("Email Valid \n");
+		} else {
+			System.out.printf("Email Invalid \n");
+		}
 	}
 
 	public static void main(String[] args) {
@@ -83,12 +95,24 @@ public class UserRegistration {
 		} else {
 			System.out.println("Phone No. Invalid");
 		}
-		System.out.println("\nEnter Password (Min 8 characters): ");
+		System.out.println(
+				"\nEnter Password (Min 8 characters with Atleast 1 Uppercase, 1 Numeric Digit, 1 Special Character): ");
 		String password = sc.next();
 		if (obj.validatePassword(password)) {
 			System.out.println("Password Valid");
 		} else {
 			System.out.println("Password Invalid");
 		}
+
+		System.out.println("\nSamples Emails to Test");
+		obj.emailTest("abc@yahoo.com");
+		obj.emailTest("abc-100@yahoo.com");
+		obj.emailTest("abc.100@yahoo.com");
+		obj.emailTest("abc111@abc.com");
+		obj.emailTest("abc-100@abc.net");
+		obj.emailTest("abc.100@abc.com.au");
+		obj.emailTest("abc@1.com");
+		obj.emailTest("abc@gmail.com.com");
+		obj.emailTest("abc+100@gmail.com");
 	}
 }
