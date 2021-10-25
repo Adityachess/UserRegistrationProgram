235c3ba377472d8eab5c0bd495aea59e358ccc3c
Adityachess <adityadone9398@gmail.com>
2021-10-26 00:32:10 +0530

    Added:User need to enter valid phone number

--- src/main/java/com/bridzelabz/userregistrationprogram/UserRegistration.java
+++ src/main/java/com/bridzelabz/userregistrationprogram/UserRegistration.java
@@ -8,8 +8,13 @@ public class UserRegistration {
 	public final String firstNamePattern = "^[A-Z]{1}[a-z]{2,}$";
 	public final String lastNamePattern = "^[A-Z]{1}[a-z]{2,}$";
 	public final String emailPattern = "[a-z0-9_]*[.a-z0-9_]*?@[a-z]*.[a-z]*[.a-z]?$";
+	public final String phonePattern = "^[0-9]{2}[0-9]{10}$";
 
-	// This method checks if the entered first name is valid
+	/**
+	 * This method checks if the entered first name is valid
+	 * 
+	 * @return
+	 */
 	public boolean validateFirstName(String fname) {
 		Pattern pattern = Pattern.compile(firstNamePattern);
 		return pattern.matcher(fname).matches();
@@ -20,13 +25,27 @@ public class UserRegistration {
 		return pattern.matcher(lname).matches();
 	}
 
-	// This method checks if the entered email is valid
+	/**
+	 * This method checks if the entered email is valid
+	 */
 	public boolean validateEmail(String email) {
 		Pattern pattern = Pattern.compile(emailPattern);
 		return pattern.matcher(email).matches();
 	}
 
-	// This is the main function
+	/**
+	 * This method checks if the entered phone number is valid
+	 */
+	public boolean validatePhone(String phone) {
+		Pattern pattern = Pattern.compile(phonePattern);
+		return pattern.matcher(phone).matches();
+	}
+
+	/**
+	 * This is the main function
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 
 		System.out.println("Enter First Name: ");
@@ -53,6 +72,14 @@ public class UserRegistration {
 			System.out.println("Email Valid");
 		} else {
 			System.out.println("Email Invalid");
+		}
+
+		System.out.println("\nEnter Phone No with Country Code: ");
+		String phone = sc.next();
+		if (obj.validatePhone(phone)) {
+			System.out.println("Phone No. Valid");
+		} else {
+			System.out.println("Phone No. Invalid");
 		}
 	}
 }
